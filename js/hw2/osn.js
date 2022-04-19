3ec2ec8bf23f6cf3377272d1b6ec3688c62d9294
Rozmaryn07 <taras.marhalevich@gmail.com>
2022-04-19 17:15:42 +0300

    hw2.1

--- js/hw2/osn.js
+++ js/hw2/osn.js
@@ -1,17 +1,18 @@
 // - Є змінна х, якій ви надаєте довільне числове значення.
-//     Якщо змінна a не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+//     Якщо змінна a не дорівнює нулю, виведіть 'Вірно',
+//     інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
 // let x = -3;
-// if ( x != 0 ) {console.log('Virno')}
+// if ( x !== 0 ) {console.log('Virno')}
 // else {console.log('NE')};
 //
 // - Дано змінну time яка рівна числу від 0 до 59.
 // Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
-// let time = (-1);
-// if ( 0 <= time, time <= 15 ) {console.log('First quarter');}
-// else if ( 15 < time, time <= 30 ) {console.log('Second quarter');}
-// else if ( 30 < time, time <= 45 ) {console.log('Third quarter');}
-// else if ( 45 < time, time <= 59 ) {console.log('Fourth quarter');}
+// let time = -12;
+// if ( 0 <= time && time <= 15 ) {console.log('First quarter');}
+// else if ( 15 < time && time <= 30 ) {console.log('Second quarter');}
+// else if ( 30 < time && time <= 45 ) {console.log('Third quarter');}
+// else if ( 45 < time && time <= 59 ) {console.log('Fourth quarter');}
 // else if ( time < 0 ) {console.log('nema');}
 // else  console.log('ne to');
 // питання!!!!
@@ -53,8 +54,8 @@
 //     - Користувач вводить або має два числа.
 //         Потрібно знайти та вивести максимальне число з тих двох .
 //         Також потрібно врахувати коли введені рівні числа.
-let x = prompt('x');
-let y = prompt('y');
+let x = +prompt('x');
+let y = +prompt('y');
 if (x>y){console.log(x) }
 else if (x<y){console.log(y)}
 else if (x=y) {console.log("рівні")};
